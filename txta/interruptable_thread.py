ef19c5282ba9c6d5d86fb7e58fe52ae7a0f80729
Mark Williams <markrwilliams@gmail.com>
2015-03-07 16:52:47 -0800

    initial commit; interruptable thread class

---
+++ txta/interruptable_thread.py
@@ -0,0 +1,84 @@
+import os
+import cffi
+import threading
+import signal
+
+
+ffi = cffi.FFI()
+
+ffi.cdef('''
+struct _cffi_pthread {
+    ...;
+};
+
+typedef struct _cffi_pthread _cffi_pthread_t;
+
+void _cffi_pthread_self(_cffi_pthread_t* thread);
+
+int _cffi_pthread_kill(_cffi_pthread_t* thread, int sig);
+''')
+
+lib = ffi.verify('''
+#include <pthread.h>
+
+struct _cffi_pthread {
+    pthread_t id;
+};
+
+typedef struct _cffi_pthread _cffi_pthread_t;
+
+void _cffi_pthread_self(_cffi_pthread_t* thread)
+{
+    thread->id = pthread_self();
+}
+
+
+int _cffi_pthread_kill(_cffi_pthread_t* thread, int sig)
+{
+    return pthread_kill(thread->id, sig);
+}
+
+''',
+                 ext_package='txta',
+                 libraries=['pthread'])
+
+
+def _pthread_self():
+    thread = ffi.new('_cffi_pthread_t*')
+    lib._cffi_pthread_self(thread)
+    return thread
+
+
+def _pthread_kill(thread, sig):
+    ret = lib._cffi_pthread_kill(thread, sig)
+    if ret:
+        raise OSError('[Errno {}] {}'.format(ret,
+                                             os.strerror(ret)))
+
+
+def noop(*args):
+    return None
+
+
+class ThreadNotStarted(Exception):
+    pass
+
+
+class InterruptableThread(threading.Thread):
+    interruptSignal = signal.SIGUSR1
+
+    def __init__(self, *args, **kwargs):
+        interruptSignal = kwargs.pop('interruptSignal', None)
+        if interruptSignal:
+            self.interruptSignal = interruptSignal
+        self._pthread_identifier = None
+        super(InterruptableThread, self).__init__(*args, **kwargs)
+
+    def _Thread__bootstrap_inner(self):
+        self._pthread_identifier = _pthread_self()
+        super(InterruptableThread, self)._Thread__bootstrap_inner()
+
+    def interrupt(self):
+        if self._pthread_identifier is None:
+            raise ThreadNotStarted
+        _pthread_kill(self._pthread_identifier, self.interruptSignal)
---
+++ txta/test/test_interruptable_thread.py
@@ -0,0 +1,42 @@
+from twisted.trial import unittest
+import time
+import signal
+import txta.interruptable_thread as T
+
+
+class TestInterruptableThread(unittest.TestCase):
+    signals = (T.InterruptableThread.interruptSignal,
+               signal.SIGUSR2)
+
+    def setUp(self):
+        self.originalHandlers = {}
+        for sig in self.signals:
+            self.originalHandlers[sig] = signal.signal(sig, T.noop)
+
+    def tearDown(self):
+        for sig, originalHandler in self.originalHandlers.items():
+            signal.signal(sig, originalHandler)
+
+    def _assertInterrupted(self, thread):
+        self.assertFalse(thread.daemon)
+        with self.assertRaises(T.ThreadNotStarted):
+            thread.interrupt()
+
+        thread.start()
+        self.assertTrue(thread.isAlive())
+
+        thread.interrupt()
+        thread.join()
+
+        self.assertFalse(thread.isAlive())
+        with self.assertRaises(OSError):
+            thread.interrupt()
+
+    def test_interrupt(self):
+        thread = T.InterruptableThread(target=lambda: time.sleep(10 << 20))
+        self._assertInterrupted(thread)
+
+    def test_interrupt_withUserSignal(self):
+        thread = T.InterruptableThread(target=lambda: time.sleep(10 << 20),
+                                       interruptSignal=signal.SIGUSR2)
+        self._assertInterrupted(thread)
